f9e5a51b667189b904798849a70b86f06de9fa70
ravila243 <55266416+ravila243@users.noreply.github.com>
2020-07-14 14:01:56 -0700

    Update test_hw.py

--- test_hw.py
+++ test_hw.py
@@ -40,27 +40,3 @@ print('')
 print('')
 
 
-
-# QUESTION 3 CHALLENGE
-def count_and_replace():
-  print('    Question 3')
-  """Count the index of the string 
-  ('12121212121212121212')
-  
-  Then replace that value in the string with the variable ('2')
-
-  Hint 1: Try using .count and .replace  
-  Hint 2: What index is '12121212121212121212'?
-
-  >>> count_and_replace()
-  22222222222222222222
-  """
-  original = '12121212121212121212'
-  new = '2'
-  #START CODE HERE
-  count = ''
-  new_message = original.replace(str(count), '') 
-  #END OF CODE
-  print(new_message)
-
-test_cases_q3(count_and_replace())
